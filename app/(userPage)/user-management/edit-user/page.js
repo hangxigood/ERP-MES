4a48e47cc81814d5d5aa5f1c0ebd00456842fa24
hangxigood <hangxigood@gmail.com>
2024-11-04 21:53:41 -0700

    Change the single quotes in JSX to &apos;

--- app/(userPage)/user-management/edit-user/page.js
+++ app/(userPage)/user-management/edit-user/page.js
@@ -85,7 +85,7 @@ export default function EditUser() {
         <form onSubmit={handleSubmit} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
           <div className="mb-4">
             <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
-              User's Email
+              User&apos;s Email
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
